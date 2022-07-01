aefe6efc956b47ef7f5d380bb2751917756a3465
Lidan-Huang <huangld0314@gmail.com>
2022-07-01 15:22:29 -0700

    added post and get an item route

--- app.js
+++ app.js
@@ -13,9 +13,24 @@ app.use(express.json());
 //process traditional form data => req.body (middleware)
 app.use(express.urlencoded({ extended: true}));
 
-app.get("/items", function (req, res) {
+//get all the items
+app.get("/", function (req, res) {
   return res.send({items});
-})
+});
+
+//add new items
+app.post("/items", function (req, res) {
+  console.log("new item:", req);
+  const newItem = req.body;
+  items.push(newItem);
+  return res.send({added: newItem});
+});
+
+//get a specific item according the given name
+app.get("/items/:name", function (req, res) {
+  const item = items.find((elem) => elem.name === req.params.name);
+  return res.send(item);
+});
 
 
 module.exports = app;
